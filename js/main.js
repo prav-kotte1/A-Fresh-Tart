0a0f03e035f9817ff5ac06908dfcad6b1f135e15
Pravallika Kotte <146621306+prav-kotte1@users.noreply.github.com>
2024-07-13 13:26:06 +0530

    Misalignment Changes

--- js/main.js
+++ js/main.js
@@ -111,10 +111,10 @@
 
 function initMap() {
     // The location of Uluru
-    var uluru = {lat: -25.344, lng: 131.036};
+    var gwalior = {lat: 26.2495, lng: 78.1741};
     // The map, centered at Uluru
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
+        document.getElementById('map'), {zoom: 7, center: gwalior});
     // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
+    var marker = new google.maps.Marker({position: gwalior, map: map});
     }
